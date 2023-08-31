ccc07e9802e6a69a44109f75a5965950c16f3451
ngochung1809 <ngochung1809@kali.kali>
2023-09-01 05:36:32 +0700

    update api gateway

---
+++ src/main/java/com/vn/config/KeySolverConfig.java
@@ -0,0 +1,16 @@
+package com.vn.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class KeySolverConfig {
+
+    @Bean
+    KeyResolver userKeySolver(){
+        return exchange -> Mono.just("userKey");
+    }
+
+}
