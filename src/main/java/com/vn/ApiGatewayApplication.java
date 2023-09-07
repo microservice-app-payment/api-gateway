572be2edc37faeeeadec4a63eed562717e977299
ngochung1809 <tongochung1809@gmail.com>
2023-09-07 23:37:36 +0700

    fix bug

--- src/main/java/com/vn/ApiGatewayApplication.java
+++ src/main/java/com/vn/ApiGatewayApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudApiGatewayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudApiGatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 }
